122ec152965e47c67eeb9664a5853d9468418ec0
mjtedder <mjtedder@gmail.com>
2019-06-08 09:37:58 -0600

    Explore arguments parameter
    Created a function that shows the arguments parameter and how it can be
    accessed similarly to JavaScript arrays

--- parameter.js
+++ parameter.js
@@ -14,3 +14,12 @@ argCheck(1, 2, 3, 4) // This logs 6 because the extra fourth argument is ignored
 // Function with missing arguments
 argCheck(1, 2) // This logs NaN (Not a Number) because param3 is undefined (1 + 2 + undefined = NaN)
 
+// The arguments parameter is a local variable accessible within all functions and contains an entry
+// for each argument passed into the function
+function argVar(param1, param2, param3) {
+    console.log(arguments.length) // Logs the number of arguments passed
+    console.log(arguments[3]) // Logs the fourth argument, following array(0) indexing notations
+}
+
+argVar(1, 2, 3, 4, 5) // Logs 5 (number of arguments), logs 4 (fourth argument)
+
